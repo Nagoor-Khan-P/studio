1931c67ba7bbc7de1131b22d6e9fb636eab1cbaf
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2026-02-01 00:02:54 +0530

    Reset migrations and switch to Postgres (Neon)

---
+++ prisma/migrations/20260131182940_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Trade" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "entryPrice" DOUBLE PRECISION NOT NULL,
+    "exitPrice" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "pnl" DOUBLE PRECISION NOT NULL,
+    "tradeDate" TIMESTAMP(3) NOT NULL,
+    "notes" TEXT,
+
+    CONSTRAINT "Trade_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Trade" ADD CONSTRAINT "Trade_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
